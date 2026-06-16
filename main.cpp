616980af763338f2436f6f474f620b4e75f5164e
PDI05 - Eduardo <pdi05@iconnect.local>
2026-06-16 15:34:49 -0300

    ponteiros ajuste

--- main.cpp
+++ main.cpp
@@ -1,37 +1,77 @@
 #include <iostream>
-#include <stack>
 #include <string>
 
 using namespace std;
 
+// Nó da pilha implementada com lista encadeada
+struct No {
+    char valor;
+    No* proximo;
+};
+
+// Pilha com ponteiros
+struct Pilha {
+    No* topo;
+
+    Pilha() : topo(nullptr) {}
+
+    bool vazia() {
+        return topo == nullptr;
+    }
+
+    void empilhar(char c) {
+        No* novo = new No();
+        novo->valor = c;
+        novo->proximo = topo;
+        topo = novo;
+    }
+
+    void desempilhar() {
+        if (vazia()) return;
+        No* temp = topo;
+        topo = topo->proximo;
+        delete temp;
+    }
+
+    // Libera toda a memória alocada
+    void limpar() {
+        while (!vazia()) {
+            desempilhar();
+        }
+    }
+};
+
 bool automatoPilha(string palavra) {
-    stack<char> pilha;
+    Pilha pilha;
     int i = 0;
 
     // Empilha para cada 'a'
-    while (i < palavra.length() && palavra[i] == 'a') {
-        pilha.push('A');
+    while (i < (int)palavra.length() && palavra[i] == 'a') {
+        pilha.empilhar('A');
         i++;
     }
 
     // Deve existir pelo menos um 'a'
-    if (pilha.empty()) {
+    if (pilha.vazia()) {
         return false;
     }
 
     // Desempilha para cada 'b'
-    while (i < palavra.length() && palavra[i] == 'b') {
-        if (pilha.empty()) {
+    while (i < (int)palavra.length() && palavra[i] == 'b') {
+        if (pilha.vazia()) {
+            pilha.limpar();
             return false;
         }
 
-        pilha.pop();
+        pilha.desempilhar();
         i++;
     }
 
     // Aceita somente se terminou a palavra
     // e a pilha ficou vazia
-    return (i == palavra.length() && pilha.empty());
+    bool aceita = (i == (int)palavra.length() && pilha.vazia());
+    pilha.limpar();
+    return aceita;
 }
 
 int main() {
